56611ca19dd483af40d63b0892a38ab39702221b
eduardo3g <eduardosbrasil10@gmail.com>
2021-03-17 17:52:47 -0300

    style: create tranasction type buttons (income and outcome) inside the modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImage from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImage from '../../assets/income.svg';
+import outcomeImage from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -35,6 +37,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           placeholder="Valor"
           type="number"
         />
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImage} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+          <button
+            type="button"
+          >
+            <img src={outcomeImage} alt="Saída"/>
+            <span>Saída</span>
+          </button>          
+        </TransactionTypeContainer>
 
         <input
           placeholder="Categoria"
