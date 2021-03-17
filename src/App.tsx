1b89eefca25628b2ed64b1996bd37de2ded2c6f0
eduardo3g <eduardosbrasil10@gmail.com>
2021-03-17 17:11:04 -0300

    feat: configure react modal in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpened,
+    setIsNewTransactionModalOpened
+  ] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpened(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpened(false);
+  }
+
   return (
     <>
-    <div className="App">
-      <Header />
-      <Dashboard />
-      <GlobalStyle />
-    </div>
+      <div className="App">
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+        <Dashboard />
+        
+        <Modal
+          isOpen={isNewTransactionModalOpened}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
+
+        <GlobalStyle />
+      </div>
     </>
   );
 }
