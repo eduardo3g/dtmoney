3614fc2b91d4eba754459c10e3799705a5e7752c
eduardo3g <eduardosbrasil10@gmail.com>
2021-03-17 17:43:09 -0300

    feat: create initial form structure inside the new transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,7 +14,26 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       isOpen={isOpen}
       onRequestClose={onRequestClose}
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          placeholder="Valor"
+          type="number"
+        />
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   );
 }
